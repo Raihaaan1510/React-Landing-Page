ecf1ca5b561a6aec58520b7ba75b2e889d0d15ca
kritik <kritikjain9@gmail.com>
2024-11-05 03:27:58 +0530

    kritik changes

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -1,29 +1,35 @@
 import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { byPrefixAndName } from '@awesome.me/kit-KIT_CODE/icons'
 
-export const Team = (props) => {
+export const HowItWorks = (props) => {
   return (
     <div id="team" className="text-center">
       <div className="container">
         <div className="col-md-8 col-md-offset-2 section-title">
           <h2>How it Works</h2>
           <p>
-          Everything is easy with us. Follow these simple steps to get started:
+            Everything is easy with us. Follow these simple steps to get started:
           </p>
         </div>
         <div id="row">
           {props.data
             ? props.data.map((d, i) => (
-                <div key={`${d.name}-${i}`} className="col-md-3 col-sm-6 team">
-                  <div className="thumbnail">
-                    {" "}
-                    <img src={d.img} alt="..." className="team-img" />
-                    <div className="caption">
-                      <h4>{d.name}</h4>
-                      <p>{d.job}</p>
-                    </div>
+              <div key={`${d.name}-${i}`} className="col-md-4 col-sm-6 team">
+                <div className="thumbnail">
+                  {" "}
+                  {/* <img src={d.img} alt="..." className="team-img" /> */}
+                  {/* <FontAwesomeIcon icon={byPrefixAndName.fas['house']} /> */}
+                  <h1><FontAwesomeIcon icon="fa-solid fa-gear" /></h1>
+                  <h1 className={d.icon}></h1>
+                  <h1 className="">1</h1>
+                  <div className="caption">
+                    <h4>{d.name}</h4>
+                    <p>{d.job}</p>
                   </div>
                 </div>
-              ))
+              </div>
+            ))
             : "loading"}
         </div>
       </div>
--- src/components/gallery.jsx
+++ src/components/gallery.jsx
@@ -6,27 +6,26 @@ export const Gallery = (props) => {
     <div id="portfolio" className="text-center">
       <div className="container">
         <div className="section-title">
-          <h2>Gallery</h2>
+          <h2>Our Projects</h2>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed
-            dapibus leonec.
+            Take a look at our work
           </p>
         </div>
         <div className="row">
           <div className="portfolio-items">
             {props.data
               ? props.data.map((d, i) => (
-                  <div
-                    key={`${d.title}-${i}`}
-                    className="col-sm-6 col-md-4 col-lg-4"
-                  >
-                    <Image
-                      title={d.title}
-                      largeImage={d.largeImage}
-                      smallImage={d.smallImage}
-                    />
-                  </div>
-                ))
+                <div
+                  key={`${d.title}-${i}`}
+                  className="col-sm-6 col-md-4 col-lg-4"
+                >
+                  <Image
+                    title={d.title}
+                    largeImage={d.largeImage}
+                    smallImage={d.smallImage}
+                  />
+                </div>
+              ))
               : "Loading..."}
           </div>
         </div>
