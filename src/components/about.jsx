36f6f0e3c367d291277843e6e709fc6581247a67
kritik <kritikjain9@gmail.com>
2024-11-12 02:57:17 +0530

    Updated why choose u

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -15,13 +15,25 @@ export const About = (props) => {
             <div className="about-text">
               <h2>ABOUT</h2>
               <p>"CIPHERSOFT  brings ideas to life by designing innovative, reliable, and future-ready software solutions.  Built from the ground up, our services are designed to empower companies to leap ahead and to meet all your digital and software needs, ensuring your business thrives in the digital age and embrace the future with confidence."</p>
-              <h3>Why Choose Us?</h3>
+              {/* <h3>Why Choose Us?</h3>
               <div className="list-style">
                 <div className="col-lg-6 col-sm-6 col-xl-22">
-                  <ul>
-                  </ul>
+                  <ul>  Innovative Solutions</ul>
+                  <ul>  Content Optimization</ul>
+                  <ul>  Expertise and Experience</ul>
+                  <ul>  Timely Delivery</ul>
+                  <ul>  Customized Solutions</ul>
+                  <ul>  SEO</ul>
                 </div>
-              </div>
+              </div> */}
+              <h3>Why Choose Us?</h3>
+              <li>Innovative Solutions</li>
+              <li>Content Optimization</li>
+              <li>Expertise and Experience</li>
+              <li>Timely Delivery</li>
+              <li>Customized Solutiomn</li>
+              <li>SEO</li>
+
             </div>
           </div>
         </div>
