91d8596af9f4a37398c1aa7aa3654afde6e36f9b
kritik <kritikjain9@gmail.com>
2024-11-12 02:08:12 +0530

    Done : Services section complete

--- src/components/services.jsx
+++ src/components/services.jsx
@@ -7,22 +7,21 @@ export const Services = (props) => {
         <div className="section-title">
           <h2>Our Services</h2>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed
-            dapibus leonec.
+            These are the services that we offer currently
           </p>
         </div>
         <div className="row">
           {props.data
             ? props.data.map((d, i) => (
-                <div key={`${d.name}-${i}`} className="col-md-4">
-                  {" "}
-                  <i className={d.icon}></i>
-                  <div className="service-desc">
-                    <h3>{d.name}</h3>
-                    <p>{d.text}</p>
-                  </div>
+              <div key={`${d.name}-${i}`} className="col-md-4">
+                {" "}
+                <i className={d.icon}></i>
+                <div className="service-desc">
+                  <h3>{d.name}</h3>
+                  {/* <p>{d.text}</p> */}
                 </div>
-              ))
+              </div>
+            ))
             : "loading"}
         </div>
       </div>
